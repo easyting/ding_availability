7b8e788ba8c9ab01398333437a7d906db5b9c197
Anatoly Muntean <am@inlead.dk>
2013-08-15 12:32:48 +0300

    case 3111 - Do not show anything if no holdings are available.

--- js/ding_availability.js
+++ js/ding_availability.js
@@ -105,10 +105,6 @@
             $('#' + id).append('<h2>' + Drupal.t('Status for the material') + '</h2>');
             $('#' + id).append(Drupal.DADB[entity_id].html);
         }
-        // if no html is given; this is exceptional situation.
-        else if (Drupal.DADB[entity_id] == undefined || Drupal.DADB[entity_id]['holdings'].length == 0) {
-          $('#' + id).append('<h2>' + Drupal.t('No holdings available') + '</h2>');
-        }
       }
 
       // Helper method to compute statuses.
